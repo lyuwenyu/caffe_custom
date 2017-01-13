aa8020fef3c01e1ded105bcac94732fc95f98254
Wenyu Lv <v-wenylv@microsoft.com>
2017-01-13 16:52:44 +0800

    bilinear interpolation and transpose

--- layer_roi_warp/roi_warp_layer.py
+++ layer_roi_warp/roi_warp_layer.py
@@ -49,10 +49,11 @@ class roi_warp_layer(caffe.Layer):
 		#print bottom[0].diff.shape
 		#print bottom[1].diff.shape
 		#print bottom[1].data.shape
-		feature_map = bottom[0].data
-		rois = bottom[1].data
+		#feature_map = bottom[0].data
+		#rois = bottom[1].data
 
 		bottom[0].diff = 0
+		bottom[1].diff = 0
 
 		for i in range(top[0].diff.shape[0]):
 			
@@ -63,29 +64,29 @@ class roi_warp_layer(caffe.Layer):
 			ih = bbox[]
 			iw = bbox[]
 
-			coor_out, coor_fea, delta_h, delta_w = self._bilinear_transpose(ih, iw, self.out_size, self.out_size)
+			coor_out, coor_fea, delta_index = self._bilinear_transpose(ih, iw, self.out_size, self.out_size)
 
 			for c in top[0].diff.shape[1]:
-
 				for k in range(len(coor_out)):
 
 					diff_top = top[0].diff[i, c, coor_out[k][0], coor_out[k][1]]
 
-					bottom[0, c, h_shift+ coor_fea[k][0],   w_shift+ coor_fea[k][1]] += (1-delta_h)* (1-delta_w)* diff_top
-					bottom[0, c, h_shift+ coor_fea[k][0]+1, w_shift+ coor_fea[k][1]] += delta_h * (1-delta_w)* diff_top
-					bottom[0, c, h_shift+ coor_fea[k][0],   w_shift+ coor_fea[k][1]+1] += (1-delta_h)* delta_w* diff_top
-					bottom[0, c, h_shift+ coor_fea[k][0]+1, w_shift+ coor_fea[k][1]+1] += delta_h * delta_w* diff_top
+					bottom[0, c, h_shift+ coor_fea[k][0],   w_shift+ coor_fea[k][1]] +=   (1-delta_index[k][0]) * (1-delta_index[k][1]) * diff_top
+					bottom[0, c, h_shift+ coor_fea[k][0]+1, w_shift+ coor_fea[k][1]] +=   delta_index[k][0] * (1-delta_index[k][1]) * diff_top
+					bottom[0, c, h_shift+ coor_fea[k][0],   w_shift+ coor_fea[k][1]+1] += (1-delta_index[k][0]) * delta_index[k][1] * diff_top
+					bottom[0, c, h_shift+ coor_fea[k][0]+1, w_shift+ coor_fea[k][1]+1] += delta_index[k][0] * delta_index[k][1] * diff_top
 
 
-	def _bilinear_transpose(self, ih, iw, oh, ow)
+			bottom[1].diff[i, ...] = 0
 
+	def _bilinear_transpose(self, ih, iw, oh, ow):
 
-		sh = ih / oh
-		sw = iw / ow
+		sh = ih*1.0 / oh
+		sw = iw*1.0 / ow
 
-		ho, wo = np.meshgrid(np.arange(self.out_size), np.arange(self.out_size))
-		h = floor(ho * sh)
-		w = floor(wo * sw)
+		ho, wo = np.meshgrid(np.arange(oh), np.arange(ow))
+		h = np.floor(ho * sh)
+		w = np.floor(wo * sw)
 
 		h = np.maximum(h, 0)
 		w = np.maximum(w, 0)
@@ -96,7 +97,19 @@ class roi_warp_layer(caffe.Layer):
 		delta_h = ho * sh - h
 		delta_w = wo * sw - w 
 
-		coor_fea = np.vstack(h.flatten(), w.flatten()).transpose()
-		coor_out = np.vstack(ho.flatten(), wo.flatten()).transpose()
+		delta_index = np.vstack([delta_h.flatten(), delta_w.flatten()]).transpose()
 
-		return coor_out, coor_fea, delta_h, delta_w
+		coor_fea = np.vstack(h.flatten(), w.flatten()).transpose().astype(np.float32)
+		coor_out = np.vstack(ho.flatten(), wo.flatten()).transpose().astype(np.float32)
+
+		return coor_out, coor_fea, delta_index
+
+
+#out = np.zeros([224,224,3], dtype=np.float32)
+#for c in range(3):
+#    for k in range(coor_out.shape[0]):
+#        out[coor_out[k][0], coor_out[k][1], c] = \
+#            img[coor_fea[k][0],   coor_fea[k][1],   c]* (1-indx[k][0]) * (1-indx[k][1]) + \
+#            img[coor_fea[k][0]+1, coor_fea[k][1],   c]* (indx[k][0]) * (1-indx[k][1]) + \
+#            img[coor_fea[k][0],   coor_fea[k][1]+1, c]* (1-indx[k][0]) * (indx[k][1]) + \
+#            img[coor_fea[k][0]+1, coor_fea[k][1]+1, c]* (indx[k][0]) * (indx[k][1])
