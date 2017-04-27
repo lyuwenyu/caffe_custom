77849747832f90dc22a4a4fea7aa408076cee7d9
Nick Lyu <lyuwenyu@users.noreply.github.com>
2017-04-27 16:14:16 +0800

    Update map_detection.py

--- detection_scripts/map_detection.py
+++ detection_scripts/map_detection.py
@@ -174,8 +174,8 @@ if __name__ == '__main__':
 
 
     import glob
-    gt_filenames = glob.glob('D:/XiChen/KITTI/training/label_02/*.txt')
-    det_filnames = glob.glob('D:/WenyuLv/workspace/py-faster-rcnn-master/test/kitti_result/vgg/train/*.txt')
+    gt_filenames = glob.glob('./KITTI/training/label_02/*.txt')
+    det_filnames = glob.glob('./workspace/py-faster-rcnn-master/test/kitti_result/vgg/train/*.txt')
 
 
     aps = []
